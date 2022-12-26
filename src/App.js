de2e61a211c25b74c44f174e175e8c02dbf2a6b9
nikhila-naidu <nikhilanaidu.kaparapu@gmail.com>
2022-12-26 23:05:51 +0530

    chore: modifying useeffect

--- src/App.js
+++ src/App.js
@@ -81,7 +81,6 @@ function App() {
         const result = await axios.get('items/todos');
         setTodos(result.data.data);
       } catch (error) {
-        setTodos([]);
         toast({
           title: 'unable to fetch data',
           position: 'top-right',
